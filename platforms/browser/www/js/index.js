3a2cfe36bc44572ba0f2e5aa24855da3319180a7
Ivan Jorge <ivan.jorge001@mymdc.net>
2017-05-18 16:07:04 -0400

    building

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -16,6 +16,11 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ //========================API Handler================================================
+
+
+
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -33,62 +38,62 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-      const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
-
-$(document).ready( () => {
-  $('#fetch-all').on('click', (e) => {
-charactersAPI.getFullList();
-  });
-
-  $('#fetch-one').on('click', (e) => {
-charactersAPI.getOneRegister($('#searchByID').val());
-  });
-
-  $('#delete-one').on('click', (e) => {
-    charactersAPI.deleteOneRegister($('#deleteOne').val());
-  });
-
-
-
-  // ===============================================================
-  $('#new-character-form').on('submit', (e) => {
-    event.preventDefault();
-var
-name = $('#name').val(),
-occupation = $('#occupation').val(),
-debt ,
-weapon = $('#weapon').val();
-
-if( $('#debt').prop("checked",true)){
-  debt = true;
-}else {
-  debt = false;
-}
-console.log(name,occupation,debt,weapon);
-charactersAPI.createOneRegister(name,occupation,debt,weapon);
-  });
-
-  $('#edit-character-form').on('submit', (e) => {
-    event.preventDefault();
-
-    var
-    one = $('#updateOne').val(),
-    name = $('#edit-name').val(),
-    occupation = $('#edit-occupation').val(),
-    debt ,
-    weapon = $('#edit-weapon').val();
-    if( $('#edit-debt').prop("checked",true)){
-      debt = true;
-    }else {
-      debt = false;
-    }
-charactersAPI.updateOneRegister(one,name,occupation,debt,weapon);
-  });
-
-});
-
-
+        app.receivedEvent();
     },
     // Update DOM on a Received Event
+    receivedEvent: function() {
+ const charactersAPI = new APIHandler("http://ih-api.herokuapp.com");
+      $(document).ready( () => {
+        $('#fetch-all').on('click', (e) => {
+      charactersAPI.getFullList();
+        });
 
+        $('#fetch-one').on('click', (e) => {
+      charactersAPI.getOneRegister($('#searchByID').val());
+        });
+
+        $('#delete-one').on('click', (e) => {
+          charactersAPI.deleteOneRegister($('#deleteOne').val());
+        });
+
+
+
+        // ===============================================================
+        $('#new-character-form').on('submit', (e) => {
+          event.preventDefault();
+      var
+      name = $('#name').val(),
+      occupation = $('#occupation').val(),
+      debt ,
+      weapon = $('#weapon').val();
+
+      if( $('#debt').prop("checked",true)){
+        debt = true;
+      }else {
+        debt = false;
+      }
+      console.log(name,occupation,debt,weapon);
+      charactersAPI.createOneRegister(name,occupation,debt,weapon);
+        });
+
+        $('#edit-character-form').on('submit', (e) => {
+          event.preventDefault();
+
+          var
+          one = $('#updateOne').val(),
+          name = $('#edit-name').val(),
+          occupation = $('#edit-occupation').val(),
+          debt ,
+          weapon = $('#edit-weapon').val();
+          if( $('#edit-debt').prop("checked",true)){
+            debt = true;
+          }else {
+            debt = false;
+          }
+      charactersAPI.updateOneRegister(one,name,occupation,debt,weapon);
+        });
+
+      });
+
+    }
 };
